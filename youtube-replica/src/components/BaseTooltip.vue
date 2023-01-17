3f5c4f41cf7ee50ebb5faeb440ebea44e2638d29
Evgeniy Zhuravlev <supreme465@gmail.com>
2023-01-17 21:25:20 +0300

    created BaseTooltip comp, fixed styles of search btn

---
+++ youtube-replica/src/components/BaseTooltip.vue
@@ -0,0 +1,51 @@
+<template>
+	<div class="relative">
+		<div
+			class="h-full"
+			@mouseenter="toggle"
+			@mouseleave="toggle"
+		>
+			<slot />
+		</div>
+  	<div v-show="isShown" :class="classes">{{text}}</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'BaseTooltip',
+	props: {
+		text: String,
+	},
+	data() {
+		return {
+			isShown: false,
+		}
+	},
+	computed: {
+		classes() {
+			return [
+					'bg-gray-600',
+					'bg-opacity-80',
+					'rounded-sm',
+					'text-white',
+					'text-xs',
+					'whitespace-nowrap',
+					'p-2',
+					'absolute',
+					'top-12',
+					'left-1/2',
+					'transform',
+					'-translate-x-1/2'
+			]
+		}
+	},
+	methods: {
+		toogle() {
+			this.isShown = !this.isShown
+		}
+	}
+}
+</script>
+
+<style scoped></style>
--- youtube-replica/src/components/header/TheSearchBtn.vue
+++ youtube-replica/src/components/header/TheSearchBtn.vue
@@ -1,19 +1,43 @@
 <template>
-  <button
-    class="flex items-center px-4 lg:px-6 py-1 text-gray-600 bg-gray-100 border border-l-0 border-gray-300 rounded-r-2xl hover:bg-gray-300 focus:outline-none"
-  >
-    <BaseIcon name="search" class="w-5 h-5" />
-  </button>
+  <BaseTooltip text="Search">
+    <button
+      :class="btnClasses"
+    >
+      <BaseIcon name="search" class="w-5 h-5" />
+    </button>
+  </BaseTooltip>
 </template>
 
 <script>
 import BaseIcon from "../BaseIcon.vue";
+import BaseTooltip from "../BaseTooltip.vue";
 
 export default {
   components: {
-    BaseIcon
+    BaseIcon,
+    BaseTooltip
+  },
+  computed: {
+    btnClasses() {
+      return [
+        'h-full',
+        'flex',
+        'items-center ',
+        'px-4 ',
+        'lg:px-6 ',
+        'py-1 ',
+        'text-gray-600 ',
+        'bg-gray-100 ',
+        'border ',
+        'border-l-0 ',
+        'border-gray-300 ',
+        'rounded-r-2xl ',
+        'hover:bg-gray-300 ',
+        'focus:outline-none',
+      ]
+    }
   }
 }
 </script>
 
-<style scoped></style>
+<style></style>
